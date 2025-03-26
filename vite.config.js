92c9d79a462f88939392f4c0e87cc5c9e5a88dc9
Hampus Farner <hampusfarner@outlook.com>
2025-03-26 13:34:08 +0100

    Install axios

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const apiUrl = env.BACKEND_URL;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env': env,
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: apiUrl,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
