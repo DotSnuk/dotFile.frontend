8bffcf199eb01974659cab0a78accdff11c284f1
Hampus Farner <hampusfarner@outlook.com>
2025-04-04 10:26:13 +0200

    Add settings with dropdown menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,9 +9,7 @@ export default function Header() {
 
   const loggedIn = (
     <>
-      <Link to='/bla'>Bla</Link>
       <MenuDropdown />
-      <input type='button' onClick={() => logout()} value='Logout' />
     </>
   );
   const notLoggedIn = (
--- src/components/MenuDropdown/MenuDropdown.jsx
+++ src/components/MenuDropdown/MenuDropdown.jsx
@@ -1,6 +1,43 @@
 import { Menu } from 'lucide-react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useUser } from '../UserContext/UserContext';
+
+const MENU_CLICKED_STATES = [
+  `transition duration-300 rotate-0`,
+  `transition duration-300 rotate-90 `,
+];
+
+const SETTINGS_CLICKED_STATES = [
+  `pointer-events-none`,
+  `opacity-100 left-0 scale-100 mt-6 pointer-events-auto`,
+];
+
+const SETTINGS_DROPDOWN = `absolute transition-all duration-300 ease-out transform opacity-0 scale-95  `;
 
 export default function MenuDropdown() {
-  return <Menu />;
+  const [clickedIndex, setClickedIndex] = useState(0);
+  const { logout } = useUser();
+
+  function toggleClassnames() {
+    setClickedIndex(value => 1 - value);
+  }
+  const settings = (
+    <nav className={SETTINGS_DROPDOWN + SETTINGS_CLICKED_STATES[clickedIndex]}>
+      <Link>Test</Link>
+      <input type='button' onClick={() => logout()} value='Logout' />
+    </nav>
+  );
+
+  return (
+    <>
+      <div className='relative left-0 flex flex-col'>
+        <Menu
+          className={MENU_CLICKED_STATES[clickedIndex]}
+          onClick={() => toggleClassnames()}
+        />
+        {settings}
+      </div>
+    </>
+  );
 }
